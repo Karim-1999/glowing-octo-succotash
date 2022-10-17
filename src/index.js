6a7a16c4d805ec38801439b7c0f5032623f01935
Zouhir Karim <105131699+Imparoprogrammazione99@users.noreply.github.com>
2022-10-17 13:40:31 +0200

    es3compo

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from "react";
+import { Hello } from "./components/hello";
 
-export class Hello extends React.Component {
-  render() {
-    return <h1>Hello, World!</h1>;
+
+
+export class App extends React.Component{
+  render(){
+    return <Hello />
   }
 }
