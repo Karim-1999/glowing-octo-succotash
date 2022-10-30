1c4851ead5950cd55b81b06826363af92dee916a
Zouhir Karim <105131699+Imparoprogrammazione99@users.noreply.github.com>
2022-10-30 08:56:08 +0100

    add es

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -5,8 +5,8 @@ export class Welcome extends React.Component {
 render() {
     return (
     <>
-            <p>Welcome, {this.props.name}!</p>
-            <Age age={this.props.age}></Age>
+        <p>Welcome, { this.props.name }!</p>
+        {this.props.age > 18 && <Age age={this.props.age} />}
     </>
     );
 }
