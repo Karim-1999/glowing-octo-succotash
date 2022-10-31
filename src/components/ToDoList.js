32a7299d9fca8cd2b6f98589145259654ff7e0fa
Zouhir Karim <105131699+Imparoprogrammazione99@users.noreply.github.com>
2022-10-31 13:58:25 +0100

    list6

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,50 +1,69 @@
 import React from "react";
-import reactDOM from 'react-dom';
+import reactDOM from "react-dom";
 
-const root = reactDOM.createRoot(document.querySelector('#root'))
+const root = reactDOM.createRoot(document.querySelector("#root"));
 export default class TodoList extends React.Component {
-    state = {
-        items : ['colazione', 'fare spesa', 'chiamare il dottore', 'pranzo', 'coding', 'cena', 'film'],
-        newItems :  ``
-    }
+  state = {
+    items: [
+      "colazione",
+      "fare spesa",
+      "chiamare il dottore",
+      "pranzo",
+      "coding",
+      "cena",
+      "film",
+    ],
+    newItems: "",
+  };
 
-    addEvent = () => {
-        this.setState({
-            items: [...this.state.items, this.state.newItems],
-                newItems  : ``  
-        })
-    }
+  addEvent = () => {
+    this.setState({
+      items: [...this.state.items, this.state.newItems],
+      newItems: "",
+    });
+  };
 
-    addItem = (event) => {
-        this.setState({
-            newItem : event.target.value
-        })
-    }
+  addItem = (event) => {
+    this.setState({
+      newItem: event.target.value,
+    });
+  };
 
+  resetAll = () => {
+    this.setState({
+      items: [""],
+    });
+  };
 
-    resetAll = (event) => {
-        this.setState({
-            items: ['']
-        })
-    } 
-
-
-render() {
-    return(
-        <>
-        
-            <h1>Cose da fare:</h1>
-            <ul>
-                {  this.state.items.map((el,i) => <li key={el+i}>{el}</li>)}
-            </ul>
-            <input type='text' onChange={this.addItem} value={this.state.newItem} />
-            <button onClick={this.addEvent} disabled={!this.state.newItems}>Aggiungi altro</button>
-            <button onClick={this.resetAll}>RESET!</button>
-        
-
-        </>
-    )
+  render() {
+    return (
+      <div>
+        <h1>Cose da fare:</h1>
+        <ul>
+          {this.state.items.map((el, i) => (
+            <div>
+              <li key={el + i}>{el}</li>
+              <button
+                onClick={() => {
+                  this.state.items.splice(i, 1);
+                  this.setState({
+                    items: this.state.items,
+                  });
+                }}
+              >
+                Elimina elemento
+              </button>
+            </div>
+          ))}
+        </ul>
+        <input type="text" onChange={this.addItem} value={this.state.newItem} />
+        <button onClick={this.addEvent} disabled={!this.state.newItems}>
+          Aggiungi altro
+        </button>
+        <button onClick={this.resetAll}>RESET!</button>
+      </div>
+    );
+  }
 }
-}
 
-root.render(<TodoList />)
+root.render(<TodoList />);
