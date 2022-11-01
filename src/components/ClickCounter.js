286cf8b3e7b4798895fa73994cea82fb6d54e59f
Zouhir Karim <105131699+Imparoprogrammazione99@users.noreply.github.com>
2022-11-01 16:01:05 +0100

    customhooks1 git commit -m customhooks1
    git commit -m customhooks1

--- src/components/ClickCounter.js
+++ src/components/ClickCounter.js
@@ -1,21 +1,17 @@
-import { useState, useEffect } from 'react';
+import React from 'react';
+import { useCounter } from './useCounter';
 
-export function ClickCounter({initialValue = 0}) {
 
-  const [count, setCount] = useState(initialValue)
-
-  useEffect(() => {
-    console.log(`The counter is now ${count}`)
-  }, [count])
-
-  function handleCounterIncrement() {
-    setCount((c) => c + 1)
-  }
-
-    return (
-      <div>
-      <h1>Count : {count}</h1>
-      <button onClick = {()=>handleCounterIncrement()}>Increment</button>
-      </div>
-    )
-  }
+export function ClickCounter({ initialValue = 0 }) {
+    const { counter, onIncrement, onDecrement, onReset } =
+      useCounter(initialValue);
+  
+      return (
+        <div>
+        <h1>Count : {counter}</h1>
+        <button onClick={onIncrement}>Increment</button>
+        <button onClick={onDecrement}>Decrement</button>
+        <button onClick={onReset}>Reset</button>
+        </div>
+      )
+    }
