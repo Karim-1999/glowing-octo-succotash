1c589ea37c478b8763b2a2ed360c0751231a49fe
Zouhir Karim <105131699+Imparoprogrammazione99@users.noreply.github.com>
2022-11-01 19:28:57 +0100

    swr-03

--- src/components/GithubUser.js
+++ src/components/GithubUser.js
@@ -3,9 +3,10 @@ import { useSWR } from "swr";
 const fetcher = (url) => fetch(url).then((respons) => respons.json())
 
 export function GithubUser() {
-    const { data, error, isLoading } = useSWR(`https://api.github.com/users`, fetcher)
+    const { data, error, isLoading, onRefresh } = useSWR(`https://api.github.com/users`, fetcher)
     return (
         <div>
+            <button onClick={onRefresh}>Refresh</button>
         {isLoading && <h3>Loading...</h3>}
         {error && <h3>An error has occurred</h3>}
         {data && !error && (
--- src/components/useGithubUser.js
+++ src/components/useGithubUser.js
@@ -4,10 +4,16 @@ import useSWR from "swr";
 const fetcher = (url) => fetch(url).then((response) => response.json())
 
 export function useGithubUser(username) {
-    const { data, error } = useSWR(username ? `https://api.github.com/users/${username}` : null, fetcher)
+    const { data, error, mutate } = useSWR(username ? `https://api.github.com/users/${username}` : null, fetcher)
+    
+    function handleRefetchUser() {
+        mutate();
+    }
+    
     return {
         users: data,
         error,
         isLoading: !data && !error,
+        onRefresh: handleRefetchUser,
       }
     }
