bdc8a4b0b0fe79b4e089ccc02077f3053249da97
Zouhir Karim <105131699+Imparoprogrammazione99@users.noreply.github.com>
2022-11-01 19:13:12 +0100

    react-router-07

--- src/app.js
+++ src/app.js
@@ -8,34 +8,32 @@ import { GithubUserList } from "./components/GithubUserList";
 
 
 
-export class App extends React.Component {
-  render() {
-    return (
-      <>
-        
-        <div>
-          <Link to="/">Home</Link>
-          <Link to="/Login">Login</Link>
-          <Link to="/">Back</Link>
-        </div>
-        <Routes>
-          <Route path="/" element={<Welcome name="Jimmy"/>} />
-          <Route path="/counter" element={<ClickCounter />} />
-          <Route path="/users" element={<GithubUserList />}>
-            <Route path="/:username" element={<ShowGithubUser />} />
-          </Route>
-          <Route
-            path="*"
-            element={
-              <div>
-                <p>Not Found!</p>
-              </div>
-            }
-          />
-        </Routes>
-      </>
-    );
-  }
+export function App() {
+  return (
+    <>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/Login">Login</Link>
+        <Link to="/">Back</Link>
+      </div>
+      <Routes>
+        <Route path="/" element={<Welcome name="Jimmy" />} />
+        <Route path="/counter" element={<ClickCounter />} />
+        <Route path="/users" element={<GithubUserList />}>
+          <Route path="/:username" element={<ShowGithubUser />} />
+          <Route index element={<h3>Add a user and select it</h3>} />
+        </Route>
+        <Route
+          path="*"
+          element={
+            <div>
+              <p>Not Found!</p>
+            </div>
+          }
+        />
+      </Routes>
+    </>
+  );
 }  
 
 /* console.log(Welcome) */
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { Root } from "./components/Root";
 import { createRoot } from "react-dom/client";
+import "./index.css";
 
 
 
-const root = createRoot (document.querySelector("#root"))
+const root = createRoot(document.getElementById("root"))
 root.render(<Root />)
